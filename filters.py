211b9d4123844c1629f22ff9bb34a5a10eb535d6
lonkaars <loek@pipeframe.xyz>
2022-08-31 09:33:09 +0200

    add more filters

--- filters.py
+++ filters.py
@@ -3,6 +3,8 @@ import re
 def tag_gen(input_str):
   input_str = input_str.lower()
   input_str = re.sub(r'aii', '', input_str)
+  input_str = re.sub(r'atd', '', input_str)
+  input_str = re.sub(r'et\/ti', '', input_str)
   input_str = re.sub(r'20\d{2}-\d{2}', '', input_str)
   input_str = re.sub(r'[pb]\d+', '', input_str)
   input_str = re.sub(r'\d', '', input_str)
@@ -11,6 +13,7 @@ def tag_gen(input_str):
   input_str = re.sub(r'stylofoon', '', input_str)
   input_str = re.sub(r'[-]', '', input_str)
   input_str = re.sub(r'\s+', ' ', input_str)
+  input_str = re.sub(r' \(c\+\+\)', '', input_str)
   return input_str.strip()
   
 def title_filter(input_str):
